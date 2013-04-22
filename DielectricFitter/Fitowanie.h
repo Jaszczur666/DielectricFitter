0b6f8321f281ff6188f1399b72675162abbc2bce
Jarosław Komar <jaszczur666@o2.pl>
2013-04-22 23:10:07 +0200

    almost working general fit

--- DielectricFitter/Fitowanie.h
+++ DielectricFitter/Fitowanie.h
@@ -307,10 +307,19 @@ std::complex<double> HNRelaxation(double frequency,MatrixXd parameters){
 	d=parameters(0)+parameters(1)/pow((1.0+pow(ii*frequency/parameters(2),parameters(3))),parameters(4));
 	return d;
 }
+
+std::complex<double> DoubleColeDavidson(double frequency,MatrixXd parameters){
+complex <double> d;
+d=parameters(0)+parameters(1)/(1.0+pow(ii*frequency/parameters(2),1-parameters(3)))+parameters(4)/(1.0+pow(ii*frequency/parameters(5),1-parameters(6)));
+return d;
+}
 std::complex<double> RelaxationFunction(int type,double frequency,MatrixXd parameters){
 	complex <double> d;
+	int parsize;
 	d=0;
-	if (type=1) d=parameters(0)+parameters(1)/(1.0+pow(ii*frequency/parameters(2),parameters(3)));
+	parsize=parameters.rows();
+	if (type=1) d=parameters(0)+parameters(1)/(1.0+pow(ii*frequency/parameters(2),1-parameters(3)));
+//	if ((type=2)&&(parsize=6))  d=DoubleColeDavidson(frequency,parameters);
 	return d;
 }
 
@@ -402,19 +411,15 @@ void FitLMGeneral(vector<double> Dataf, vector<double>Dataep, vector<double> Dat
 		{
 			lambda=lambda/8.0;
 		}
-		if (parameters(2)<0) parameters(2)=-parameters(2);
-		if (parameters(3)<0) parameters(3)=0;
-		if (parameters(3)>1) parameters(3)=0;
-
+		
 	}
 	size=parameters.rows();
 	size2=Dataf.size();
-	cout << "rozmiary ="<<size<<" "<<size2<<endl;
 	error=(Hessiandiag.inverse().diagonal()*chi2/(size2-size));
 	for (i=0;i<size;i++) error(i)=sqrt(error(i));
-	cout<<error<<endl<< "----------------------------------------------------- "<<endl;
+	//cout<<error<<endl<< "----------------------------------------------------- "<<endl;
 	cout <<chi2<<endl;
 	end=clock();
-	cout <<(double(end - start) / CLOCKS_PER_SEC)<<" "<< CLOCKS_PER_SEC<<endl;
+	cout <<(double(end - start) / CLOCKS_PER_SEC);//<<" "<< CLOCKS_PER_SEC<<endl;
 	return;
 }
